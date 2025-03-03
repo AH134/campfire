1c67bfaa332d32a0ea8e8755074b3dfa5d92d5de
AH134 <github.com.uo5n8@aleeas.com>
2025-03-02 19:19:35 -0800

    added header and footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+import IconLink from "./lib/components/IconLink.svelte"
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<header
+    class="flex items-center justify-between border-b border-b-zinc-200 bg-white p-2 align-middle shadow"
+>
+    <div>
+        <a class="flex items-center gap-1 align-middle" href="/">
+            <img class="h-8 w-8" src="/bonfire.png" alt="logo" />
+            <h1 class="text-lg font-semibold">Campfire</h1>
+        </a>
+    </div>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <nav>
+        <ul>
+            <li>
+                <IconLink
+                    name="github"
+                    href="https://github.com/AH134/campfire"
+                    awesomeIcon="fa-brands fa-github fa-xl"
+                />
+            </li>
+        </ul>
+    </nav>
+</header>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<main class="flex-1 p-2"></main>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<footer
+    class=" mt-auto min-h-10 border-t border-t-zinc-200 bg-white p-2 text-center"
+>
+    <span>
+        Created with
+        <a
+            class="text-blue-600 hover:text-zinc-800"
+            href="https://svelte.dev"
+            target="_blank">svelte</a
+        >,
+        <a
+            class="text-blue-600 hover:text-zinc-800"
+            href="https://tailwindcss.com"
+            target="_blank">tailwindcss</a
+        >
+        &
+        <a
+            class="text-blue-600 hover:text-zinc-800"
+            href="https://fontawesome.com"
+            target="_blank">font awesome</a
+        >
+    </span>
+</footer>
---
+++ src/lib/components/IconLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+type IconProps = {
+    name: string
+    href: string
+    awesomeIcon: string
+}
+
+let { name, href, awesomeIcon }: IconProps = $props()
+</script>
+
+<a
+    class="inline-flex aspect-square min-h-6 min-w-6 items-center justify-center rounded p-1 hover:bg-zinc-200"
+    href={href}
+    target="_blank"
+    aria-label="{name} link"
+>
+    <i class={awesomeIcon}></i>
+</a>
