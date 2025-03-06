628c285ea3673cc8188408bb35ec8c002c105028
AH134 <github.com.uo5n8@aleeas.com>
2025-03-06 15:22:11 -0800

    Changed data folder path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 VOLUME_PATH="/var/www/html/data"
+DEFAULT_DATA_PATH="/default-data/"
 
 if [ -z "$(ls -A $VOLUME_PATH)" ]; then
     echo "Initializing volume with default data..."
-    cp -r /data/* $VOLUME_PATH/
+    cp -r $DEFAULT_DATA_PATH/* $VOLUME_PATH/
 fi
 
 exec "$@"
